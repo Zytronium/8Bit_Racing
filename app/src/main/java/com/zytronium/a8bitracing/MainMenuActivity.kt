4116f32c0c5e4d67204fdd0ec81e1b0d72a386a3
Zytronium <dastelljes@outlook.com>
2024-10-24 17:27:13 -0500

    Started polishing statistics screen and fixed crash when the saved current theme is unknown.

--- app/src/main/java/com/zytronium/a8bitracing/MainMenuActivity.kt
+++ app/src/main/java/com/zytronium/a8bitracing/MainMenuActivity.kt
@@ -45,7 +45,7 @@ class MainMenuActivity : AppCompatActivity(), Application.ActivityLifecycleCallb
         difficultyBar = findViewById(R.id.difficulty_bar)
         themeText = findViewById(R.id.theme_txt2)
         difficultyNameText = findViewById(R.id.difficulty_name_txt)
-        themeText.text = getCurrentThemeName()
+//        themeText.text = getCurrentThemeName()
 
         fs()
 
@@ -80,7 +80,7 @@ class MainMenuActivity : AppCompatActivity(), Application.ActivityLifecycleCallb
 
     private fun updateBackground() {
         findViewById<ConstraintLayout>(R.id.main).setBackgroundResource(
-            CurrentTheme.theme!!.backgroundTextureBlurred
+            CurrentTheme.theme?.backgroundTextureBlurred ?: themes["Race Track"]!!.backgroundTextureBlurred
         )
     }
 
