bfb8d69bd3da7ac103010f5d9a9c745697ccb66a
Zytronium <dastelljes@outlook.com>
2024-10-23 23:27:52 -0500

    made it not fade to fast music in rainbow kart theme and changed app version name

--- app/src/main/java/com/zytronium/a8bitracing/MainActivity.kt
+++ app/src/main/java/com/zytronium/a8bitracing/MainActivity.kt
@@ -237,8 +237,8 @@ class MainActivity : AppCompatActivity(), Application.ActivityLifecycleCallbacks
                     play()
             }
         }, adjustedGameSpeed())
-        println("speed: ${adjustedGameSpeed()}")
-        if(adjustedGameSpeed() <= 310 && (fastMusic == null || !fastMusic!!.isPlaying) && score > 3) {
+//        println("speed: ${adjustedGameSpeed()}")
+        if(CurrentTheme.theme?.name != "Rainbow Kart" && (adjustedGameSpeed() <= 310 && (fastMusic == null || !fastMusic!!.isPlaying) && score > 3)) {
             if(slowMusic!!.isPlaying && !musicFaded) {
                 fadeSlowMusic()
             }
@@ -916,7 +916,7 @@ class MainActivity : AppCompatActivity(), Application.ActivityLifecycleCallbacks
         println("Activity Resumed")
 //        if(!paused) {
             if(!progressing && gamePlaying && !paused) {
-                if(adjustedGameSpeed() > 310 && difficulty != 4) {
+                if(CurrentTheme.theme?.name == "Rainbow Kart" || (adjustedGameSpeed() > 310 && difficulty != 4)) {
                     slowMusic!!.start()
                 } else if(score > 5 || difficulty == 4) {
                     playFastMusic()
